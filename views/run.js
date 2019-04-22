78389d5118953879e3c32da23d82f8f6cc3c8522
masterviana <masterviana@gmail.com>
2019-04-22 10:53:38 +0100

    force index.html root file

--- views/run.js
+++ views/run.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 
 //app.use(express.static(__dirname)); // Current directory is root
-app.use('/',express.static(path.join(__dirname, 'app'))); //  "public" off of current is root
+app.use(express.static(path.join(__dirname, 'app'))); //  "public" off of current is root
 
 
 let port = process.env.PORT || 4201
