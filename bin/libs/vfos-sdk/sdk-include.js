36cd3740c41cfca07908f18ff0ce61532120895d
masterviana <masterviana@gmail.com>
2019-04-23 01:42:18 +0100

    fix gen script of SDK

--- bin/libs/vfos-sdk/sdk-include.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  messaging: require("./components/vfos-messaging/index.js"),
-  security: require('../lib/Security'),
-  datastorageNosql: require('./components/restheart-js-client'),
-  datastorageRelational: require('./components/datastorage-nodejs-client'),
-  enablerFramework: require('')
-}
